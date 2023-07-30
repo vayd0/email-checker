d1d4569940af15f31d6abf1fa7ec203b4bf5edc6
vayd0 <kiiroxyz@gmail.com>
2023-07-30 04:24:57 +0200

    email checker

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const MailChecker = require("mailchecker");
+const { validate } = require("email-validator");
+const dns = require("dns");
+const { promisify } = require("util");
 const fs = require("fs");
 require("colors");
 
-console.log("Si vous n'avez aucune réponse pensez à remplir emails.txt".yellow);
-const emails = fs.readFileSync("./emails.txt", "utf-8").split("\n");
+const resolveMx = promisify(dns.resolveMx);
 
-emails.forEach((data) => {
-  const res = MailChecker.isValid(data);
-  if (res == false) return;
-  console.warn("[ VALIDE ] ".green + data);
-});
+async function checkEmailExistence(email) {
+  if (!email) {
+    return false;
+  }
+
+  const isValid = validate(email.trim());
+  if (isValid) {
+    const domain = email.split("@")[1];
+    if (!domain) return false;
+    try {
+      const mxRecords = await resolveMx(domain);
+      const smtpServer = mxRecords[0].exchange;
+      return true;
+    } catch (error) {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+async function checkEmails(emailsToCheck) {
+  for (const email of emailsToCheck) {
+    const result = await checkEmailExistence(email.replace("\r", ""));
+
+    if (result) return console.log("[ VALIDE ]".green, email);
+  }
+}
+
+const emailsToCheck = fs.readFileSync("./emails.txt", "utf-8").split("\n");
+
+checkEmails(emailsToCheck);
